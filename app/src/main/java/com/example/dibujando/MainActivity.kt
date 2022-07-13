2200e875d2d9776c007fd849b2c8500b1d3f7136
joulsCarvajal <juliancarvajaldiaz1992@outlook.com>
2022-07-13 16:14:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dibujando/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.dibujando
+
+import android.graphics.Color
+import android.graphics.Paint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import com.example.dibujando.PaintView.Companion.colorList
+import com.example.dibujando.PaintView.Companion.currentBrush
+import com.example.dibujando.PaintView.Companion.pathList
+import kotlin.io.path.Path
+
+class MainActivity : AppCompatActivity() {
+
+    //Necesitaremos dos cosas importantes para dibujar, el path y el actual paint
+    //El companion object es lo que conocimos en Java como public static, que nos permitirá llamar
+    //las variables desde cualquier parte del código como public.
+
+    companion object{
+        var path = android.graphics.Path() //De android graphics no de Java
+        var paintBrush = Paint() //De android graphics
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide() // Esto permite que se oculte el nombre de la app
+
+        //Inicializamos todos nuestros views que tenemos en nuestro activity_main.xml
+        val redBtn = findViewById<ImageButton>(R.id.redColor)
+        val blueBtn = findViewById<ImageButton>(R.id.blueColor)
+        val blackBtn = findViewById<ImageButton>(R.id.blackColor)
+        val whiteBtn = findViewById<ImageButton>(R.id.whiteColor)
+
+        redBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.RED
+            currentColor(paintBrush.color)
+        }
+
+        blueBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLUE
+            currentColor(paintBrush.color)
+        }
+
+        blackBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLACK
+            currentColor(paintBrush.color)
+        }
+
+        whiteBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            pathList.clear()
+            colorList.clear()
+            path.reset()
+        }
+    }
+
+    private fun currentColor(color: Int){
+        currentBrush = color
+        path = android.graphics.Path()
+    }
+}
---
+++ app/src/main/java/com/example/dibujando/PaintView.kt
@@ -0,0 +1,83 @@
+//Esta clase será la responsable de todo el dibujo
+package com.example.dibujando
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import com.example.dibujando.MainActivity.Companion.paintBrush
+import com.example.dibujando.MainActivity.Companion.path
+
+
+
+//Ponemos el view, sin embargo el constructor no viene en las ayudas de a.s., entonces toca ir
+//a google y buscar el "contructor of view in android studio kotlin"
+
+class PaintView: View {
+
+    var params: ViewGroup.LayoutParams? = null // Con esto sabe cual es la altura y el ancho de nuestro lienzo con respecto al diseño principal
+
+    companion object{
+        var pathList = ArrayList<Path>() //El path list sirve como un contenedor de las lineas que haremos
+        var colorList = ArrayDeque<Int>()
+        var currentBrush = Color.BLACK
+    }
+
+    constructor(context: Context) : this(context, null){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    //Inicializamos la brocha o el pincel o laìz o como quiera llamarse
+    private fun init(){
+        paintBrush.isAntiAlias = true //Permite que la textura sea suave o rigida
+        paintBrush.color = currentBrush
+        paintBrush.style = Paint.Style.STROKE
+        paintBrush.strokeJoin = Paint.Join.ROUND //Con este finalizamos el path del dibujo
+        paintBrush.strokeWidth = 8f
+
+        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    //Inicializamos el registro del movimiento del dedo sobr la pantalla para get de la información
+    // Obtendremos las coordenadas x y y
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        var x = event.x
+        var y = event.y
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> {
+                path.moveTo(x,y)
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(x,y)
+                pathList.add(path)
+                colorList.add(currentBrush)
+            }
+            else -> return false
+        }
+        postInvalidate()
+        return false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        for(i in pathList.indices){
+            paintBrush.setColor(colorList[i])
+            canvas.drawPath(pathList[i], paintBrush)
+            invalidate()
+        }
+    }
+
+}
